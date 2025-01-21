1be022921b16e4086e9b5c818fd3ef9a4599eb6d
Firazaba0369 <123702516+Firazaba0369@users.noreply.github.com>
2025-01-21 13:29:31 -0800

    test mustang.bmp image file

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from cipher import *
 
 key = generate_aes_key(16)
 iv = get_random_bytes(16)
+#cp-logo
 encrypt_image("cp-logo.bmp", "ECB", key)
 decrypt_image("cp-logo_ECB_encrypted.bmp", "ECB", key)
 encrypt_image("cp-logo.bmp", "CBC", key, iv)
 decrypt_image("cp-logo_CBC_encrypted.bmp", "CBC", key, iv)
+#mustang
+encrypt_image("mustang.bmp", "ECB", key)
+decrypt_image("mustang_ECB_encrypted.bmp", "ECB", key)
+encrypt_image("mustang.bmp", "CBC", key, iv)
+decrypt_image("mustang_CBC_encrypted.bmp", "CBC", key, iv)
 # cipher_text = submit("You’re the man now, dog", key, iv)
 # print(verify(cipher_text, key, iv))
